0974f11734fba90bd233f46bd5a3e05c1189cdd5
Neutix <martin.wedvik@gmail.com>
2021-11-01 19:10:40 +0100

    initial project

---
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,11 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class HttpClientTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(10, 2*5);
+    }
+}
