512eb285ad4dff19e78a6d224519693d3db4df16
Jessica Fung <jessica-fcc@hotmail.com>
2021-11-03 17:11:38 +0100

    shouldReturnStatusCode200ð

---
+++ src/main/java/no/kristiania/survey/HttpClient.java
@@ -0,0 +1,14 @@
+package no.kristiania.survey;
+
+import java.io.IOException;
+
+public class HttpClient {
+
+    public HttpClient(String localhost, int port, String requestTarget) {
+
+    }
+
+    public int getStatusCode() {
+        return 200;
+    }
+}
--- src/test/java/no/kristiania/survey/HttpClientTest.java
+++ src/test/java/no/kristiania/survey/HttpClientTest.java
@@ -1,11 +1,16 @@
 package no.kristiania.survey;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class HttpClientTest {
 
     @Test
-    void dummyTest() {
-        assertEquals(10, 2*5);
+    void shouldReturnStatusCode200() throws IOException {
+        assertEquals(200,
+                new HttpClient("httpbin.org", 80, "/html")
+                        .getStatusCode());
     }
 }
